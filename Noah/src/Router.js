ec72245208766f01665f0ce12e82ec31e9a007f1
timucini <timurburk@googlemail.com>
2018-11-23 21:11:53 +0100

    Data-Integration with simple QuestionPage with QuestionText+Questionoptions

--- Noah/src/Router.js
+++ Noah/src/Router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import startPage from './components/startPage';
 import QuestionPage from './components/QuestionPage';
+import QuestionTest from './components/QuestionTest';
 
 
 const RouterComponent = () => {
@@ -10,6 +11,7 @@ const RouterComponent = () => {
             <Scene key="root">
                 <Scene key="startPage" component={startPage} title="start Page" initial />
                     <Scene key="QuestionPage" component={QuestionPage} title="Fragebogen" />
+                        <Scene key="QuestionTest" component={QuestionTest} title="Testbogen" /> 
             </Scene>
         </Router>
     );
--- Noah/src/components/QuestionPage.js
+++ Noah/src/components/QuestionPage.js
@@ -1,15 +1,100 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { 
+    View,
+    Text,
+    TouchableOpacity,
+    ScrollView,
+    StyleSheet,
+    Button
+    } from 'react-native';
+import jsondata from '../assets/datasrc/structure_final.json';
+
 
 class QuestionPage extends Component {
-    render() {
-        return (
-            <View>
-                <Text>Template für Frage</Text>
-            </View>
-        );
+    constructor(props) {
+        super(props);
+        this.qno = 0;
+        this.score = 0;
+        
+        const jdata = jsondata.fb1;
+        const arrnew = Object.keys(jdata).map(function(k) { return jdata[k]; });
+        this.state = {
+          question: arrnew[this.qno].frageText,
+          options: arrnew[this.qno].options,
+          correctoption: arrnew[this.qno].correctAnswer,
+          countCheck: 0
+        };
     }
-}
+    render() {
+        
+        const currentOptions = this.state.options;
+        const options = Object.keys(currentOptions).map(function(k) {
+            return (
+                <View key={k} style={{ margin: 10 }}>
+ 
+                    <Button 
+                        onPress={
+                        (status) => this.answer(status, k)} 
+                        title={currentOptions[k]} 
+                    />
+         
+                </View>);
+            });
 
+        return (
+           
+           <ScrollView style={{ backgroundColor: '#F5FCFF', paddingTop: 10 }}>
+            <View style={styles.container}>
+       
+            <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center' }} />
+       
+            <View style={styles.oval} >
+              <Text style={styles.welcome}>
+                {this.state.question}
+              </Text>
+           </View>
+              <View>
+              { options }
+              </View>
+              <View style={{ flexDirection: 'row' }}>
+            {/*   <Button
+                onPress={() => this.prev()}
+                title="Prev"
+                color="#841584"
+              />
+              <View style={{margin:15}} />*/}
+       
+              <TouchableOpacity onPress={() => this.next()} >
+                <View style={{ paddingTop: 5, paddingBottom: 5, paddingRight: 20, paddingLeft: 20, borderRadius: 10, backgroundColor: 'green' }} />
+              </TouchableOpacity >
+              </View>
+              </View>
+            </ScrollView>
+          );
+        }
+      }
+         
+const styles = StyleSheet.create({
+ 
+    oval: {
+    
+    borderRadius: 20,
+    backgroundColor: 'green'
+    },
+    container: {
+      flex: 1,
+      alignItems: 'center'
+    },
+    welcome: {
+      fontSize: 20,
+      margin: 15,
+      color: 'white'
+    },
+    instructions: {
+      textAlign: 'center',
+      color: '#333333',
+      marginBottom: 5,
+    },
+  });
 export default QuestionPage;
 
