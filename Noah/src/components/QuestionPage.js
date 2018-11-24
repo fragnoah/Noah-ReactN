e414036819cd93803982a94d3ce141b9fcb8c19c
timucini <timurburk@googlemail.com>
2018-11-24 01:21:39 +0100

    First logic implementation + navigation between different questions

--- Noah/src/components/QuestionPage.js
+++ Noah/src/components/QuestionPage.js
@@ -7,7 +7,8 @@ import {
     StyleSheet,
     Button
     } from 'react-native';
-import jsondata from '../assets/datasrc/structure_final.json';
+import Animbutton from '../components/common/AnimButton';
+import jsondata from '../assets/datasrc/test.json';
 
 
 class QuestionPage extends Component {
@@ -15,69 +16,147 @@ class QuestionPage extends Component {
         super(props);
         this.qno = 0;
         this.score = 0;
+        this.selectedAns= 0;
         
+        // hier muss aus redux fb1 bzw fb2 xyz abgerufen
         const jdata = jsondata.fb1;
-        const arrnew = Object.keys(jdata).map(function(k) { return jdata[k]; });
+        this.arrnew = Object.keys(jdata).map(k => jdata[k]);
         this.state = {
-          question: arrnew[this.qno].frageText,
-          options: arrnew[this.qno].options,
-          correctoption: arrnew[this.qno].correctAnswer,
-          countCheck: 0
+            id: this.arrnew[this.qno].id,
+            question: this.arrnew[this.qno].frageText,
+            options: this.arrnew[this.qno].options,
+            correctoption: this.arrnew[this.qno].correctAnswer,
+            categories: this.arrnew[this.qno].category
         };
+        /* durch redux Mitgabe von: 
+        FB,score, richtig beantworte Fragen, falsch beantwortete Fragen, gehighlightete Fragen
+        */
     }
-    render() {
-        
+    prev() {
+        if (this.qno > 0) {
+          this.qno--;
+          this.setState({
+            id: this.arrnew[this.qno].id,
+            question: this.arrnew[this.qno].frageText,
+            options: this.arrnew[this.qno].options,
+            correctoption: this.arrnew[this.qno].correctAnswer,
+            countCheck: 0
+        });
+        }
+      }
+    next() {
+        if (this.qno < this.arrnew.length - 1) {
+            if (this.selectedAns === this.state.correctoption) {
+                this.score += 1;
+            }
+            this.qno++;
+            this.setState({
+                countCheck: 0,
+                id: this.arrnew[this.qno].id, 
+                question: this.arrnew[this.qno].frageText,
+                options: this.arrnew[this.qno].options,
+                correctoption: this.arrnew[this.qno].correctAnswer });
+        } else {
+            this.props.quizFinish(this.score);
+        }
+    }
+    answer(ans) {
+        this.selectedAns = ans;
+    } 
+    /*_answer(status, ans) { 
+        if (status === true) {
+            const count = this.state.countCheck + 1;
+            this.setState({ countCheck: count });
+            if (ans === this.state.correctoption) {
+                this.score += 1;
+            }
+        }
+    } /*
+     TO-DO 
+     1.nur ein Button clickbar machen -> vielleicht mit counCheck arbeiten? :/
+     2.bei next button neu rendern
+     3.score den Kategorien anpassen
+     4.bei zurück umgekehrt von next machen -
+     > wenn antwort falsch bleibt score gleich, bei antwort richtig wir score einen abgezogen ;) 
+    
+    */
+     render() {
+        let _this = this;
         const currentOptions = this.state.options;
-        const options = Object.keys(currentOptions).map(function(k) {
+        const options = Object.keys(currentOptions).map( function (k) {
+        return (
+                <View key={k} style={{ margin: 10 }}>
+        
+            <Animbutton countCheck={_this.state.countCheck} onColor={'green'} effect={'tada'} _onPress={() => _this.answer(k)} text={currentOptions[k]} />
+     
+          </View>
+        );
+        });
+        /*
+        const currentOptions = this.state.options;
+        const options = Object.keys(currentOptions).map(function (k) {
             return (
                 <View key={k} style={{ margin: 10 }}>
  
-                    <Button 
+                    <Button
+                        countCheck={this.state.countCheck} 
                         onPress={
                         (status) => this.answer(status, k)} 
                         title={currentOptions[k]} 
                     />
-         
                 </View>);
             });
-
+            */
         return (
-           
            <ScrollView style={{ backgroundColor: '#F5FCFF', paddingTop: 10 }}>
-            <View style={styles.container}>
+                <View style={styles.container}>
+                    <View 
+                    style={{
+                        flex: 1,
+                        flexDirection: 'column',
+                        justifyContent: 'space-between',
+                        alignItems: 'center' }} 
+                    />
        
-            <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center' }} />
-       
-            <View style={styles.oval} >
-              <Text style={styles.welcome}>
-                {this.state.question}
-              </Text>
-           </View>
-              <View>
-              { options }
-              </View>
-              <View style={{ flexDirection: 'row' }}>
-            {/*   <Button
-                onPress={() => this.prev()}
-                title="Prev"
-                color="#841584"
-              />
-              <View style={{margin:15}} />*/}
-       
-              <TouchableOpacity onPress={() => this.next()} >
-                <View style={{ paddingTop: 5, paddingBottom: 5, paddingRight: 20, paddingLeft: 20, borderRadius: 10, backgroundColor: 'green' }} />
-              </TouchableOpacity >
-              </View>
-              </View>
-            </ScrollView>
-          );
-        }
-      }
-         
+                    <View style={styles.oval} >
+                        <Text style={styles.welcome}>
+                            {this.state.question}
+                        </Text>
+                    </View>
+                    <View>
+                        { options }
+                    </View>
+                <View style={{ flexDirection: 'row' }}>
+                    <Button
+                     onPress={() => this.prev()}
+                     title="Prev"
+                     color="#841584"
+                    />
+                <View style={{ margin: 15 }} />                 
+                    <TouchableOpacity onPress={() => this.next()} >
+                        <View 
+                            style={{ paddingTop: 5,
+                            paddingBottom: 5,
+                            paddingRight: 20,
+                            paddingLeft: 20,
+                            borderRadius: 10,
+                            backgroundColor: 'green' }} 
+                        />
+                        <Text>Next</Text>
+                    </TouchableOpacity >
+                    <Text>
+                        {this.score}
+                    </Text>
+                </View>
+            </View>
+        </ScrollView>
+        );
+    }
+}
 const styles = StyleSheet.create({
  
     oval: {
-    
+    width: 350,
     borderRadius: 20,
     backgroundColor: 'green'
     },
--- Noah/src/components/startPage.js
+++ Noah/src/components/startPage.js
@@ -3,6 +3,25 @@ import { View, Text, Button } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 
 class startPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            fb: 0
+        };
+    // hier muss vermutlich redux angewannt werden
+    }
+    getFb1() {
+        this.setState({
+            fb: 'fb1'
+        });
+        Actions.QuestionPage();
+    }
+    getFb2() {
+        this.setState({
+            fb: 'fb2'
+        });
+        Actions.QuestionPage();
+    }
     render() {
         return (
             <View>
@@ -11,8 +30,13 @@ class startPage extends Component {
                 Nachfolgende Szene ist die QuestionPage für ein Fragebogen
             </Text>
             <Button
-                onPress={() => Actions.QuestionPage()}
-                title="Test-Fragebogen"
+                onPress={() => this.getFb1()}
+                title="Fragebogen 1"
+                color="#841584"
+            />
+            <Button
+                onPress={() => this.getFb2()}
+                title="Fragebogen 2"
                 color="#841584"
             />
         </View>
