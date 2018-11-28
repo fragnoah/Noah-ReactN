6bc5636a5ffed05da485f9480dec6228a097a0c2
Matthias Cohn <matthias.cohn@googlemail.com>
2018-11-28 11:10:13 +0100

    Update MyListItem.js

--- Noah/src/components/common/MyListItem.js
+++ Noah/src/components/common/MyListItem.js
@@ -18,32 +18,6 @@ const ListItem = (props) => {
     );
 };
 
-/*
-class ListItem { children } extends Component {
-  onRowPress() {
-    
-    // TODO: Implement Kapitel
-    Actions.employeeEdit({ employee: this.props.employee });
-  }
-
-  render() {
-    const { name } = this.props.employee;
-
-    return (
-      <TouchableWithoutFeedback onPress={this.onRowPress.bind(this)}>
-        <View>
-          <CardSection>
-            <Text style={styles.titleStyle}>
-              {name}
-            </Text>
-          </CardSection>
-        </View>
-      </TouchableWithoutFeedback>
-    );
-  }
-}
-*/
-
 const styles = {
   titleStyle: {
     fontSize: 18,
