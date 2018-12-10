723c674718055129bc9d1ab4d488c7fbdba5a5b9
Matthias Cohn <matthias.cohn@googlemail.com>
2018-12-10 22:41:21 +0100

    now

--- Noah/src/actions/index.js
+++ Noah/src/actions/index.js
@@ -1,3 +1,45 @@
 export * from './NavActions';
 export * from './LearnActions';
+/*
 export * from './Quizactions';
+*/
+export const selectFb = (fb) => {
+    return {
+      type: 'select_fb',
+      payload: fb
+    };
+};
+
+export const selectAnswer = (antwort) => {
+  return {
+    type: 'select_answer',
+    payload: antwort
+  };
+};
+
+export const updateAnswer = (antwort, qno) => {
+  return {
+    type: 'update_answer',
+    payload: antwort,
+    index: qno
+  };
+};
+
+export const resetFb = () => {
+  return {
+    type: 'reset_fb'
+  };
+};
+
+export const getBasisScore = (basisScore) => {
+  return {
+    type: 'get_BasisScore',
+    payload: basisScore
+  };
+};
+export const getSpezScore = (spezScore) => {
+  return {
+    type: 'get_SpezScore',
+    payload: spezScore
+  };
+};
