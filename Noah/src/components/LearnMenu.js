7189ba8779abb5ddea8790e8782e27bc3b01e3af
Matthias Cohn <matthias.cohn@googlemail.com>
2018-12-20 20:15:13 +0100

    Fragenpool mit Bildern

--- Noah/src/components/LearnMenu.js
+++ Noah/src/components/LearnMenu.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, SectionList, Text } from 'react-native';
 import { ListItem } from './common/MyListItem';
 import * as actions from '../actions';
 
 class LearnMenu extends Component {
+
     render() {
-        const q = { quest: 'Test' };
-        console.log(q); 
         return (
             <ScrollView>
                 <ListItem children="Basisfragen" onPress={actions.toLearnBasicQuestions} />
                 <ListItem children="Binnenfragen" onPress={actions.toLearnBinnenQuestions} />
                 <ListItem children="Segelfragen" onPress={actions.toLearnSegelQuestions} />
                 <ListItem children="Glossar" onPress={actions.toGlossar} />
-
             </ScrollView>
         );
     }
