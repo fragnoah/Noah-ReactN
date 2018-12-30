6938c45ea8499b17128808198c0041d1963fd8eb
Matthias Cohn <matthias.cohn@googlemail.com>
2018-12-30 13:10:05 +0100

    iOSFix Complete

--- Noah/src/components/RepeatPage.js
+++ Noah/src/components/RepeatPage.js
@@ -4,7 +4,8 @@ import {
     Text,
     ScrollView,
     StyleSheet,
-    //Button
+    Platform,
+    ImageBackground
     } from 'react-native';
 
 import { connect } from 'react-redux';
@@ -109,7 +110,8 @@ class RepeatPage extends Component {
             />  
         );         
     }
-     render() {
+
+    renderContent() {
         const radioProps = [
             { label: this.arrnew[this.antworten[this.state.qno]].options.option1,
                 value: 'option1' },
@@ -220,7 +222,32 @@ class RepeatPage extends Component {
              
         );
     }
+
+    render() {
+        if (Platform.OS === 'ios') {
+            return (
+                <ImageBackground
+                    source={iosFix.path}
+                    style={iosFix.style}
+                >
+                    {this.renderContent()}
+                </ImageBackground>
+            );
+        }
+        return (
+            this.renderContent()
+        );
+    }
 }
+
+const iosFix = {
+    style: {
+        flex: 1,
+        resizeMode: 'cover',
+    },
+    path: require('../assets/img/NOAH_Wallpaper.png'),
+};
+
 const styles = StyleSheet.create({
  
     flashMessage: {
