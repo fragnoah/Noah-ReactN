2f9040bf2a81ae8730837f6aec0ab117b5b99d8b
Yen Luu <46230688+fragnoah@users.noreply.github.com>
2019-01-19 01:58:07 +0100

    Update messages.js
    Quality check: Edited alert message

--- Noah/src/actions/messages.js
+++ Noah/src/actions/messages.js
@@ -9,10 +9,11 @@ export const startInfo = () => {
         'Information',
         'In der Prüfung SBF Binnen Segeln werden 30 Fragen aus dem Fragenkatalog gestellt.'
         + 'Um die Prüfung zu bestehen, musst du 7 Basisfragen, 23 Spezialfragen (21 Binnenfragen und 2 Segelfragen) richtig beantworten.'
-        + ' Jeder Fragebogen simuliert den exakten Ablauf der Prüfungsfragen. Bei der Prüfung hast du 60 Minuten Zeit, um alle Fragen zu beantworten.'
+        + ' Jede Prüfungsbogen Variante simuliert den exakten Ablauf der Prüfung für den SBF unter Segeln. Bei der Prüfung hast du 60 Minuten Zeit, um alle Fragen zu beantworten.'
         + ' Am Ende eines Durchlaufs siehst du, ob du eine Prüfung bestanden hättest. Tipp: Nutze das Pin- Symbol,'
         + ' um Fragen für später zu speichern und noch einmal zu wiederholen. Optional: Damit du alle Fragen mindestens'
-        + ' einmal im Prüfungsmodus richtig beantwortet hast, empfehlen wir dir, alle Fragebogen zu durchlaufen.Viel Erfolg!',
+        + ' einmal im Prüfungsmodus richtig beantwortet hast, empfehlen wir dir, alle Prüfungsbogen Varianten einmal zu durchlaufen.'
+        + ' Wir wünschen dir viel Erfolg!',
         [
         { text: 'Abbrechen', onPress: () => false, style: 'cancel' },
         { text: 'OK', onPress: () => true },
